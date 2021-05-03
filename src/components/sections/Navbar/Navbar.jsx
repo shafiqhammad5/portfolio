676639524fdfc0774c32862f63b300a9708a5535
Shafiq Hammad <shafiqhammad@Shafiqs-MacBook-Pro.local>
2021-05-03 09:56:58 +0600

    first update

--- src/components/sections/Navbar/Navbar.jsx
+++ src/components/sections/Navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { BrowserRouter as Router, Link, Route, Switch } from "react-router-dom";
 import About from "../../pages/About/About";
 import Blog from "../../pages/Blog/Blog";
@@ -7,66 +7,46 @@ import Portfolio from "../../pages/Portfolio/Portfolio";
 import Service from "../../pages/Service/Service";
 
 const Navbar = () => {
-  const [isActive, setIsActive] = useState(" ");
-  const menuActiveHandler = (e, menu) => {
-    // e.target.href ===
-    setIsActive(!menu);
-    console.log(e.target);
-  };
-
-  let classes;
-  if (isActive) {
-    classes = `text-purple-600`;
-  } else {
-    classes = `text-gray-800`;
-  }
-  console.log(isActive, classes);
-
   return (
     <Router>
       <nav className="mx-4 px-4 py-8">
         <ul className="flex">
           <li className="mr-10">
             <Link
-              onClick={() => menuActiveHandler(true)}
               to="/"
-              className={classes + ` text-medium hover:text-purple-600`}
+              className="text-gray-800 text-medium hover:text-purple-600"
             >
               About
             </Link>
           </li>
           <li className="mr-10">
             <Link
-              onClick={() => menuActiveHandler(false)}
               to="/services"
-              className={classes + ` text-medium hover:text-purple-600`}
+              className="text-gray-800 text-medium hover:text-purple-600"
             >
               Services
             </Link>
           </li>
           <li className="mr-10">
             <Link
-              onClick={() => menuActiveHandler("works")}
               to="/works"
-              className={classes}
+              className="text-gray-800 text-medium hover:text-purple-600"
             >
               Works
             </Link>
           </li>
           <li className="mr-10">
             <Link
-              onClick={() => menuActiveHandler("blogs")}
               to="/blogs"
-              className={classes}
+              className="text-gray-800 text-medium hover:text-purple-600"
             >
               Blogs
             </Link>
           </li>
           <li className="mr-10">
             <Link
-              onClick={() => menuActiveHandler("contact")}
               to="/contact"
-              className={classes}
+              className="text-gray-800 text-medium hover:text-purple-600"
             >
               Contact
             </Link>
